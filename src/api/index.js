dfc01a080c233c5de3ea1578b39c974d2fa92106
Aleksey Kopasov <alesey.k1@gmail.com>
2022-07-09 00:48:59 +0300

    WIP: исправляет ошибки CORS

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
+// import axios from 'axios';
 // eslint-disable-next-lin e import/named
-import { APP_ID, REDIRECT_URI, VK_AUTH_URI } from '@/utils/consts';
+// import { APP_ID, REDIRECT_URI, VK_AUTH_URI } from '@/utils/consts';
 
 // eslint-disable-next-line max-len
-// const TOKEN = 'vk1.a.ejlwfjmUJU-ndJv0T4y1G-MOkmUR8Y9sRoanj4R-ad-y_3vHqV4U808nvvB6ihBlWAXOGOzMDnc-D8vEsZqEFaHc5_J2C287fbHtf-J3hRGaCP6u3iVLmTu1xwsEKe5cggRKWSfR-7S0l8BeE9X_x203KGWh-N4zCzgt5Q_kB9K75uiuNnLDDWon9kJno0iq';
-
+// const TOKEN =
 /*
 * 1. авторизоваться
 * 2. если есть авторизация - получать пользователя
@@ -14,17 +13,16 @@ import { APP_ID, REDIRECT_URI, VK_AUTH_URI } from '@/utils/consts';
 // eslint-disable-next-line import/no-cycle
 import store from '@/store';
 // import { getData, storeData } from '@/utils/storage';
-const path = `${VK_AUTH_URI}?`
-  + `${APP_ID}&`
-  + 'display=page&'
-  + `${REDIRECT_URI}&`
-  + '&response_type=token';
+
+// const path = `${VK_AUTH_URI}?`
+//   + `${APP_ID}&`
+//   + 'display=page&'
+//   + `${REDIRECT_URI}&`
+//   + 'response_type=token';
+
+// const PATH = 'https://oauth.vk.com/authorize?client_id=8210774&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.131&state=123456';
 
 const auth = () => {
-  axios.get(`${path}`)
-    .then((res) => {
-      console.log(res);
-    });
   // storeData(userId, accessToken);
 };
 
@@ -33,16 +31,7 @@ const searchUser = () => {
   if (!isAuthorization) {
     auth();
   }
-
   // поиск пользователя по id или имени
-  // axios.get('https://oauth.vk.com/authorize?'
-  //   + 'client_id=1&'
-  //   + 'display=page&'
-  //   + 'redirect_uri=http://localhost:8080'
-  //   + '&response_type=token')
-  //   .then((res) => {
-  //     console.log(res);
-  //   });
 };
 
 export default searchUser;
--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: {
-      '^/': {
-        target: 'https://oauth.vk.com/authorize',
-        pathRewrite: { '^/': '' },
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    proxy: 'https://oauth.vk.com/authorize',
   },
 });
